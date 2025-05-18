03a1b36f421771491ce12be5a7e5323927ceed79
Wmm.malshan <111701987+fireteambot@users.noreply.github.com>
2025-05-18 17:30:44 +0530

    Create bt.sh

---
+++ bt.sh
@@ -0,0 +1,16 @@
+echo "Blocking torrent ports..."
+# Common torrent ports
+sudo ufw deny out 6881:6889/tcp
+sudo ufw deny out 6881:6889/udp
+sudo ufw deny out 6969/tcp
+sudo ufw deny out 51413
+sudo ufw deny out 1337/tcp
+sudo ufw deny out 2710/tcp
+sudo ufw deny out 8999/tcp
+sudo ufw deny out 8999/udp
+sudo ufw deny out 42069/tcp
+sudo ufw deny out 42069/udp
+sudo ufw deny out 16881/tcp
+sudo ufw deny out 16881/udp
+sudo ufw deny out 6880:6999/tcp
+sudo ufw deny out 6880:6999/udp
