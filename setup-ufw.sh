dc98867d77170588f0fb7b2ff8d6daabf9709ee9
Wmm.malshan <111701987+fireteambot@users.noreply.github.com>
2025-05-18 14:16:07 +0530

    Update setup-ufw.sh
    block torrent

--- setup-ufw.sh
+++ setup-ufw.sh
@@ -24,9 +24,22 @@ for port in 2052 2053 2086 2087 2095 2096 8443 8880 8080; do
   sudo ufw allow out $port/tcp
 done
 
+echo "Blocking torrent ports..."
+sudo ufw deny out 6881:6889/tcp
+sudo ufw deny out 6881:6889/udp
+sudo ufw deny out 6969/tcp
+sudo ufw deny out 51413
+sudo ufw deny out 1337/tcp
+sudo ufw deny out 2710/tcp
+sudo ufw deny out 49152:65535/tcp  
+sudo ufw deny out 49152:65535/udp
+sudo ufw deny out 10000:65535/udp
+sudo ufw deny out 1024:65535/udp
+sudo ufw deny out 6880:6999/tcp
+sudo ufw deny out 6880:6999/udp
+
 echo "Allowing messaging & media apps..."
 sudo ufw allow out 443/udp
-sudo ufw allow out 443/tcp
 sudo ufw allow out 3478/udp
 sudo ufw allow out 19302/udp
 sudo ufw allow out 3478:3481/udp
@@ -39,12 +52,6 @@ sudo ufw allow out 5000:6000/udp
 echo "Allowing outbound for service on port 8000..."
 sudo ufw allow out 8000/tcp
 sudo ufw allow out 8000/udp
-
-echo "Blocking torrent ports..."
-sudo ufw deny out 6881:6889/tcp
-sudo ufw deny out 6881:6889/udp
-sudo ufw deny out 6969/tcp
-sudo ufw deny out 51413
 
 echo "Enabling UFW..."
 sudo ufw --force enable
